c759e4a53f2ddcf620d826b46a6ba6cc1dac68da
Inderpreet02 <inder192002@gmail.com>
2021-04-24 00:37:28 +0530

    did some baic things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Exclusive from './components/Exclusive';
 
 function App() {
   return (
     <div className="App">
-      
+      <Exclusive/>
     </div>
   );
 }
---
+++ src/components/Exclusive.js
@@ -0,0 +1,38 @@
+import "./Exclusive.css"
+
+const Exclusive = () => {
+    return (
+        <div className="exclusive">
+            <h1>Exclusively for you!</h1>
+            <div className="exclusive__container">
+                <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/910f83b0-581b-464f-85ed-69ac2866ba9b/Earring--420x528Px.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-910f83b0-581b-464f-85ed-69ac2866ba9b-nwXd0PG" alt=""/>
+                <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/961d58f0-ec12-4e1c-8573-4deb5d93517f/Pendant-01-420x528Px.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-961d58f0-ec12-4e1c-8573-4deb5d93517f-nwXd0PG" alt=""/>
+                <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/3ab40bbc-4d1b-43ff-9a76-94144a7a4a68/Pendant-420x528Px.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-3ab40bbc-4d1b-43ff-9a76-94144a7a4a68-nwXd0PG" alt=""/>
+            </div>
+
+            <div className="shop__byGender">
+                <h1>Shop By Gender</h1>
+                <div className="shop_byGenderContainer">
+                    <div className="shop__byGenderCard">
+                        <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/ae6a3bbf-07dc-43ae-a529-94ea24d8bc4a/Gender+Icons_child.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-ae6a3bbf-07dc-43ae-a529-94ea24d8bc4a-niwxvs9" alt=""/>
+                        <h5>KIDS</h5>
+                    </div>
+                    <div className="shop__byGenderCard">
+                        <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/f0879ce9-df52-4388-ab79-3ece2b21a482/Gender+Icons+Man.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-f0879ce9-df52-4388-ab79-3ece2b21a482-niwxwDt" alt=""/>
+                        <h5>MEN</h5>
+                    </div>
+                    <div className="shop__byGenderCard">
+                        <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/16282cdf-557f-454b-83d5-ddc33976af9e/Gender+Icons+Lady.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-16282cdf-557f-454b-83d5-ddc33976af9e-niwxm-q" alt=""/>
+                        <h5>WOMEN</h5>
+                    </div>
+                    <div className="shop__byGenderCard">
+                        <img src="https://tanishq.co.in/wps/wcm/connect/tanishq/628c5886-9c2d-40da-8d24-07336efe70e1/Gender+Icons_couple.png?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_90IA1H80O0RT10QIMVSTFU3006-628c5886-9c2d-40da-8d24-07336efe70e1-neT--Ng" alt=""/>
+                        <h5>PAIR</h5>
+                    </div>
+            </div>
+                </div>
+        </div>
+    );
+}
+ 
+export default Exclusive;
